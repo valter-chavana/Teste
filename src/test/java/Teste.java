a0d2438672faf33d0acb748a040b8a8422255287
Valter Pedro Júnior Chavana (DSI) <Valter.Chavana@BCI.CO.MZ>
2021-04-28 08:44:58 +0200

    ading test anotation

--- src/test/java/Teste.java
+++ src/test/java/Teste.java
@@ -1,23 +1,29 @@
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+
 public class Teste {
+    public static WebDriver driver;
     public static void main(String[] args) {
         // declaration and instantiation of objects/variables
-        System.setProperty("webdriver.gecko.driver","C:\\Users\\valter.chavana\\Desktop\\DEV\\ValidaçãoAllure\\src\\test\\java\\resourses\\geckodriver25.exe");
-        WebDriver driver = new FirefoxDriver();
+        System.setProperty("webdriver.gecko.driver","C:\\Users\\valter.chavana\\Desktop\\DEV\\ValidateAllure\\src\\test\\java\\resourses\\geckodriver25.exe");
+        driver = new FirefoxDriver();
         //comment the above 2 lines and uncomment below 2 lines to use Chrome
         //System.setProperty("webdriver.chrome.driver","G:\\chromedriver.exe");
         //WebDriver driver = new ChromeDriver();
 
         String baseUrl = "http://demo.guru99.com/test/newtours/";
-        String expectedTitle = "Welcome: Mercury Tours";
-        String actualTitle = "";
-
         // launch Fire fox and direct it to the Base URL
         driver.get(baseUrl);
 
-        // get the actual value of the title
+        testExemplo();
+
+    }
+    @Test
+    static void testExemplo(){
+        String expectedTitle = "Welcome: Mercury Tours";
+        String actualTitle = "";
         actualTitle = driver.getTitle();
 
         /*
